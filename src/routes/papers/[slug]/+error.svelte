b224c8a6d0f9d38e9046cec808ad3b2db07c5220
Dylan Cashman <depcashman@gmail.com>
2024-11-20 11:34:30 -0500

    Remove slugged routes, update people to include lane, and add preview task

--- src/routes/papers/[slug]/+error.svelte
+++
@@ -1,8 +0,0 @@
-<script lang="ts">
-	import { page } from '$app/stores';
-	console.log('error');
-</script>
-
-{#if $page.error && $page.error.message}
-	<h1>{$page.status}: {$page.error.message}</h1>
-{/if}
